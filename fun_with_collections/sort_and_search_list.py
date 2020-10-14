4678d4b33e021eaf837860db69da3e72c4714dd1
ComputerUser158 <oneringraw@gmail.com>
2020-10-13 20:10:17 -0500

    First failing commit Sort and search list assignment

---
+++ fun_with_collections/sort_and_search_list.py
@@ -0,0 +1,48 @@
+"""
+Author: Rawley Collins
+Program: sort_and_search_list.py
+
+"""
+
+
+def sort_list(newList):
+	pass
+	"""newList.sort()
+	return newList # I returned a list because I needed to print out the list after it was sorted"""
+
+
+def search_list(newList):
+	pass
+	"""userInput = int(input("input what you would like to find in the list: "))
+	if userInput in newList:
+		return newList.index(userInput)
+	else:
+		return -1
+"""
+
+
+def make_list():
+	u_list = []
+	counter = 0
+	for i in range(0, 3):
+		while counter < 3:
+			try:
+				u_input = int(input("enter a number between 1 and 50:"))
+				if u_input < 1 or u_input > 50:
+					raise ValueError
+				else:
+					u_list.insert(counter, u_input)
+					counter = counter + 1
+			except ValueError:
+				print("That wasn't valid input try again,")
+	return u_list
+
+
+if __name__ == '__main__':
+	# test_list = make_list()
+	# sort_list()
+	newList = make_list()
+	print(newList)
+	# print(sort_list(newList))
+	# userInput = int(input("input what you would like to find in the list: "))
+	print(search_list(newList))
---
+++ test_collections/test_sort_and_search_list.py
@@ -0,0 +1,21 @@
+import unittest
+from unittest.mock import patch
+from fun_with_collections import sort_and_search_list
+
+
+class MyTestCase(unittest.TestCase):
+    @patch('fun_with_collections.sort_and_search_list', returnvalue=5)
+    def test_search_list_found(self, input):
+        result = sort_and_search_list.search_list([5, 7, 8])
+        self.assertEqual(result, 0)
+
+
+class MyTestCase(unittest.TestCase):
+    @patch('fun_with_collections.sort_and_search_list', returnvalue=5)
+    def test_search_list_not_found(self, input):
+        result = sort_and_search_list.search_list([6, 7, 8])
+        self.assertEqual(result, -1)
+
+
+if __name__ == '__main__':
+    unittest.main()
